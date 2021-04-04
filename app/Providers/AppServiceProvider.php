629a6c27796accd9a688a38b8735ee4b036e04a1
DarknessNinjaBR <darkenessninjabr@hotmail.com>
2021-04-04 18:19:19 -0300

    Front menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\Facades\View;
+use App\Models\Page;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //Menu
         Paginator::useBootstrap();
+
+        $frontMenu = [
+            '/' => 'Home'
+        ];
+
+        $pages = Page::all();
+
+        foreach($pages as $page){
+            $frontMenu[$page['slug']] = $page['title'];
+        }
+
+        View::share("front_menu", $frontMenu);
+
     }
 }
